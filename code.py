19d0dbd4d62b1d5fa682ab5bec511a67aad24cd1
Samuel Jun <126729269+smjun0106@users.noreply.github.com>
2023-07-30 23:42:40 -0400

    Update code.py

--- code.py
+++ code.py
@@ -8,13 +8,13 @@ messages = [{"role": "system", "content": "You are an expert on logical and crit
 def CustomChatGPT(input):
     messages.append({"role": "user", "content": input})
     response = openai.ChatCompletion.create(
-        model = "gpt-4",
+        model = "gpt-3.5-turbo",
         messages = messages
     )
     ChatGPT_reply = response["choices"][0]["message"]["content"]
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
 
-demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Thinking Pro")
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "ThoughtCoach")
 
 demo.launch(share=True)
